f510c0102d4ccfda76a249dcfc5c2c132c96b6ea
parthcs-secondary <parthcs626@gmail.com>
2026-09-23 20:04:35 +0000

    added files

--- providers.tf
+++ providers.tf
@@ -2,7 +2,11 @@
 
 terraform {
   required_version = ">= 1.6.0"
-
+  backend "s3" {
+    bucket = "tf-state-zaidyn"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
